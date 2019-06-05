6f57a38f4f9c0f739e230ef1bdb219851c49ac58
Ryan Tharp <neuro@interx.net>
2019-06-05 15:29:44 -0700

    first pass at download-binaries

--- index.js
+++ index.js
@@ -75,6 +75,84 @@ const lib = require(__dirname + '/lib')
 var logo = lib.getLogo('L A U N C H E R   v e r s i o n   v version')
 console.log(logo.replace(/version/, VERSION.toString().split('').join(' ')))
 
+const http = require('http')
+const https = require('https')
+const urlparser = require('url')
+
+function getFileSizeSync(path) {
+  const stats = fs.statSync(path);
+  return stats.size;
+}
+
+function downloadGithubFile(dest, url, cb) {
+
+  const urlDetails = urlparser.parse(url)
+  //console.log('httpGet url', urlDetails)
+  //console.log('httpGet', url)
+  var protoClient = http
+  if (urlDetails.protocol == 'https:') {
+    protoClient = https
+  }
+  // well somehow this can get hung on macos
+  var abort = false
+  var watchdog = setInterval(function () {
+    if (shuttingDown) {
+      //if (cb) cb()
+      // [', url, ']
+      console.log('hung httpGet but have shutdown request, calling back early and setting abort flag')
+      clearInterval(watchdog)
+      abort = true
+      cb(false)
+      return
+    }
+  }, 2 * 60 * 60 * 1000)
+  protoClient.get({
+    hostname: urlDetails.hostname,
+    protocol: urlDetails.protocol,
+    port: urlDetails.port,
+    path: urlDetails.path,
+    timeout: 5000,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 Loki-launcher/' + VERSION
+    }
+  }, (resp) => {
+    //log('httpGet setting up handlers')
+    clearInterval(watchdog)
+    if (resp.statusCode == 302) {
+      console.log('Got redirect to', resp.headers.location)
+      downloadGithubFile(dest, resp.headers.location, cb)
+      return
+    }
+    var file = fs.createWriteStream(dest, { encoding: 'binary' })
+    resp.setEncoding('binary')
+    const len = parseInt(resp.headers['content-length'], 10)
+    var downloaded = 0
+    var lastPer = 0
+    resp.on('data', function(chunk) {
+      downloaded += chunk.length
+      var tenPer = parseInt(10 * downloaded / len, 10)
+      //console.log('tenper', tenper, downloaded / len)
+      if (tenPer != lastPer) {
+        console.log('Downloaded', tenPer * 10, '%')
+        lastPer = tenPer
+      }
+    })
+    resp.pipe(file);
+    file.on('finish', function() {
+      console.log('File download ', downloaded, '/', len, 'bytes of', url, 'complete')
+      if (downloaded < len) {
+        console.log('file is incomplete')
+      }
+      file.close(cb)
+    })
+  }).on("error", (err) => {
+    console.error("downloadFile Error: " + err.message, 'port', urlDetails.port)
+    //console.log('err', err)
+    cb()
+  })
+}
+
+
 switch(mode) {
   case 'start':
     require('./start')(args, __filename)
@@ -109,6 +187,81 @@ switch(mode) {
   case 'args-debug':
     console.log('in :', process.argv)
     console.log('out:', args)
+  break;
+  case 'download-binaries':
+    const lib = require('./lib')
+    const lokinet = require('./lokinet')
+
+    var running = lib.getProcessState()
+    if (running.lokid) {
+      console.log('lokid is running, request shutdown')
+      process.kill(pids.lokid, 'SIGINT')
+      // should be down by the time the file downloads...
+    }
+    lokinet.mkDirByPathSync('/opt/loki-launcher/bin')
+    const github_url = 'https://api.github.com/repos/loki-project/loki/releases/latest'
+    lokinet.httpGet(github_url, function(json) {
+      try {
+        var data = JSON.parse(json)
+        var search = 'UNKNOWN'
+        if (os.platform() == 'darwin') search = 'osx'
+        else
+        if (os.platform() == 'linux') search = 'linux'
+        else {
+          console.log('Sorry, platform', os.platform(), 'is not currently supported, please let us know you would like us to support this platform by opening an issue on github: https://github.com/loki-project/loki-launcher/issues')
+          process.exit()
+        }
+        var searchRE = new RegExp(search, 'i');
+        const pathUtil = require('path')
+        for(var i in data.assets) {
+          var asset = data.assets[i]
+          //console.log(i, 'asset', asset.browser_download_url)
+          if (asset.browser_download_url.match(searchRE) && asset.browser_download_url.match(/\.zip/i)) {
+            const baseZipDir = pathUtil.basename(asset.browser_download_url, '.zip')
+            console.log('Will download', asset.browser_download_url)
+            var tmpPath = '/tmp/loki-launcher_binaryDownload-' + lokinet.randomString(8) + '.zip'
+            //console.log('downloading to tmp file', tmpPath)
+            downloadGithubFile(tmpPath, asset.browser_download_url, function(result) {
+              if (result !== undefined) {
+                console.log('something went wrong with download, try again later or check with us')
+                process.exit()
+              }
+              console.log('result is', result)
+              if (asset.browser_download_url.match(/\.zip/i)) {
+                const { exec } = require('child_process');
+
+                function waitForLokidToBeDeadAndExtract() {
+                  running = lib.getProcessState()
+                  if (running.lokid) {
+                    console.log('waiting 5s for lokid to quit...')
+                    setTimeout(waitForLokidToBeDeadAndExtract, 5000)
+                    return
+                  }
+                  exec('tar xvf '+tmpPath+' --strip-components=1 -C /opt/loki-launcher/bin '+baseZipDir+'/lokid', (err, stdout, stderr) => {
+                    // delete tmp file
+                    if (1) {
+                      console.log('cleaning up', tmpPath)
+                      fs.unlinkSync(tmpPath)
+                    }
+                    if (err) {
+                      console.error('file extract error', err)
+                      return
+                    }
+                    console.log('stdout', stdout)
+                    console.log('lokid extracted to /opt/loki-launcher/bin', getFileSizeSync('/opt/loki-launcher/bin/lokid'), 'bytes extracted')
+                  })
+                }
+                waitForLokidToBeDeadAndExtract()
+              }
+            })
+          }
+        }
+      } catch(e) {
+        console.log('json', json)
+        console.log('error with', github_url, e)
+        process.exit()
+      }
+    })
   break;
   default:
     console.log(`
