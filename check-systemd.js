a53bbc9f7c7afb82a7a75caa55fe8c9f67d092f9
Ryan Tharp <neuro@interx.net>
2019-06-06 23:18:52 +0000

    implement most

--- check-systemd.js
+++ check-systemd.js
@@ -1,15 +1,38 @@
 // no npm!
 const fs = require('fs')
 
-if (fs.existsSync('/etc/systemd/system/lokid.service')) {
-  // read file
-  // rewrite
-  // save
-  // reload
-}
 
-function start() {
-//
+function start(config, entrypoint) {
+  if (fs.existsSync('/etc/systemd/system/lokid.service')) {
+    console.log('detected lokid.service')
+    // read file
+    const service_bytes = fs.readFileSync('/etc/systemd/system/lokid.service')
+    var lines = service_bytes.toString().split(/\n/)
+    var nLines = []
+    var needsUpdate = false
+    for(var i in lines) {
+      var tline = lines[i].trim()
+      if (tline.match(/ExecStart/)) {
+        //console.log('ExecStart', tline)
+        if (tline.match(/lokid/)) {
+          console.log('ExecStart uses lokid directly')
+          needsUpdate = true
+          // replace ExecStart
+          tline = 'ExecStart=' + entrypoint + ' systemd-start';
+        }
+      }
+      nLines.push(tline)
+    }
+    if (needsUpdate) {
+      if (process.getuid() != 0) {
+        console.warn('can not update your lokid.service, not running as root, please run with sudo')
+      } else {
+        console.log('updating lokid.service')
+        var newBytes = nLines.join("\n")
+        fs.writeFileSync('/etc/systemd/system/lokid.service', newBytes)
+      }
+    }
+  }
 }
 
 module.exports = {
