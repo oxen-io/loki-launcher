97f76f0f561627b1b2f3b4b17d419ccf4ff53386
Ryan Tharp <neuro@interx.net>
2019-06-11 16:49:10 -0700

    forget new username to uid decoder files

---
+++ get-uid.js
@@ -0,0 +1,21 @@
+if (module !== require.main) {
+  throw new Error("This file should not be loaded with require()")
+}
+
+if (!process.getuid || !process.getgid) {
+  throw new Error("this file should not be called without uid/gid support")
+}
+
+var argv = process.argv.slice(2)
+  , user = argv[0] || process.getuid()
+
+if (!isNaN(user)) user = +user
+
+console.error([user])
+
+try {
+  process.setuid(user)
+  console.log(JSON.stringify({uid:+process.getuid()}))
+} catch (ex) {
+  console.log(JSON.stringify({error:ex.message,errno:ex.errno}))
+}
