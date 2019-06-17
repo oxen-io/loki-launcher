e1120f2ff32521ce324eee07275c4c4b35752b84
Ryan Tharp <neuro@interx.net>
2019-06-16 23:56:05 -0700

    rewriteServiceFile() refactor and add message if file DNE

--- check-systemd.js
+++ check-systemd.js
@@ -2,37 +2,50 @@
 const fs = require('fs')
 
 
-function start(config, entrypoint) {
-  if (fs.existsSync('/etc/systemd/system/lokid.service')) {
-    console.log('detected lokid.service')
-    // read file
-    const service_bytes = fs.readFileSync('/etc/systemd/system/lokid.service')
-    var lines = service_bytes.toString().split(/\n/)
-    var nLines = []
-    var needsUpdate = false
-    for(var i in lines) {
-      var tline = lines[i].trim()
-      if (tline.match(/ExecStart/)) {
-        //console.log('ExecStart', tline)
-        if (tline.match(/lokid/)) {
-          console.log('ExecStart uses lokid directly')
-          needsUpdate = true
-          // replace ExecStart
-          tline = 'ExecStart=' + entrypoint + ' systemd-start';
-        }
+function rewriteServiceFile(serviceFile) {
+  console.log('detected', serviceFile)
+  // read file
+  const service_bytes = fs.readFileSync(serviceFile)
+  var lines = service_bytes.toString().split(/\n/)
+  var nLines = []
+  var needsUpdate = false
+  for(var i in lines) {
+    var tline = lines[i].trim()
+    if (tline.match(/ExecStart/)) {
+      //console.log('ExecStart', tline)
+      if (tline.match(/lokid/)) {
+        console.log('ExecStart uses lokid directly')
+        needsUpdate = true
+        // replace ExecStart
+        tline = 'ExecStart=' + entrypoint + ' systemd-start';
       }
-      nLines.push(tline)
     }
-    if (needsUpdate) {
-      if (process.getuid() != 0) {
-        console.warn('can not update your lokid.service, not running as root, please run with sudo')
-      } else {
-        console.log('updating lokid.service')
-        var newBytes = nLines.join("\n")
-        fs.writeFileSync('/etc/systemd/system/lokid.service', newBytes)
-      }
+    nLines.push(tline)
+  }
+  if (needsUpdate) {
+    if (process.getuid() != 0) {
+      console.warn('can not update your lokid.service, not running as root, please run with sudo')
+    } else {
+      console.log('updating lokid.service')
+      var newBytes = nLines.join("\n")
+      fs.writeFileSync('/etc/systemd/system/lokid.service', newBytes)
     }
   }
+}
+
+function start(config, entrypoint) {
+  if (fs.existsSync('/etc/systemd/system/lokid.service')) {
+    rewriteServiceFile('/etc/systemd/system/lokid.service')
+  } else {
+    console.debug('/etc/systemd/system/lokid.service does not exist.')
+    console.error('You may not be running your Service Node as a system Service, please follow the full guide to reconfigure your node')
+  }
+  // https://discordapp.com/channels/408081923835953153/583133711407513630/590010043907440656
+  /*
+  if (fs.existsSync('/lib/systemd/system/loki-node.service')) {
+    rewriteServiceFile('/lib/systemd/system/loki-node.service')
+  }
+  */
 }
 
 module.exports = {
