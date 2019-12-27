f5e2f57507c252d5ed0bea7013812135c2b749e0
Ryan Tharp <neuro@interx.net>
2019-12-27 08:09:57 +0000

    socket server make ECONNRESET remove you from client list

--- daemon.js
+++ daemon.js
@@ -1106,6 +1106,14 @@ function startLokid(config, args) {
           c.write('SOCKETERR: ' + JSON.stringify(err))
         } else {
           console.log('Not connected, SOCKETERR:', JSON.stringify(err))
+          if (err.code === 'ECONNRESET') {
+            // make sure we remove ourself from broadcasts (lokid stdout writes)...
+            var idx = connections.indexOf(c)
+            if (idx != -1) {
+              connections.splice(idx, 1)
+            }
+            // leave it to the client to reconnect
+          }
         }
       })
       c.on('data', (data) => {
